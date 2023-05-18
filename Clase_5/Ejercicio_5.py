ac966dac6dd898dd0749f0603f8bc37ddc3fb127
rog777 <dfmartinezfa@gmail.com>
2023-05-18 00:57:11 -0500

    Update

---
+++ Clase_5/Ejercicio_5.py
@@ -0,0 +1,25 @@
+def bisiesto(año):
+
+    bisiesto = (int(input("ingresa un año: ")))
+        
+    if (año / 4 == 0 and año / 100 != 0) or año / 400 == 0:
+        print("Es bisiesto")
+    
+    else:
+        print("No es bisiesto")
+
+bisiesto =()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
